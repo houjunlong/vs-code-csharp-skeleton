7d6f4dbcc0d6117c8e038f6e611f4252ee46870e
Ralf Kruse <vec2k4@gmail.com>
2015-05-06 21:46:57 +0200

    Project setup

---
+++ CSharpSkeleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpSkeleton
+{
+    internal class Program
+    {
+        private static List<int> _list = new List<int>();
+        
+        private static void Main(string[] args)
+        {
+            var rnd = new Random(DateTime.Now.Millisecond);
+            for (var i=0; i<100; i++)
+            {
+                var element = rnd.Next(1,100);
+                _list.Add(element);
+            }
+            
+            var q = from x in _list
+                    where x % 2 == 0
+                    select x;
+                    
+            foreach (var x in q)
+                Console.WriteLine(x);
+        }
+    }
+}
